c55461100703ca4c216f56e2e9c7b985b31b3e13
madinajabrayil <56426429+madinajabrayil@users.noreply.github.com>
2021-07-07 00:01:49 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-// import { BrowserRouter as Router, Route,Switch } from "react-router-dom";
 import './App.css';
 import Home from './components/Home/Home';
-// import Menu from './components/Menu/Menu'
 import Education from './components/Education/Education';
 import Skills from './components/Skills/Skills';
 import Contact from './components/Contact/Contact';
 import About from './components/About/About';
 import Portfolio from './components/Portfolio/Portfolio';
-// import StartsBackground from './components/Home/StarsBackground/StarsBackground'
+import Menu from './components/Menu/Menu';
 
 
 function App() {
-    return ( 
-    <div>
-        <Home/>
-        <About/>
-        <Skills/>
-        <Education/>
-        <Portfolio/>
-        <Contact/>
-    </div>  
+
+    return (
+        <div>
+            <Home/>
+            <Menu/>
+            <About/>
+            <Skills/>
+            <Education/>
+            <Portfolio/>
+            <Contact/>
+        </div>
     );
 
 
