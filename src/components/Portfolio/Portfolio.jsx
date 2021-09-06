5ecfc1cc344419d518e6035d6b336bcffd938dd9
madinajabrayil <56426429+madinajabrayil@users.noreply.github.com>
2021-09-06 23:28:33 +0400

    Update Portfolio.jsx

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -5,6 +5,7 @@ import demo2 from '../../images/demo2.png';
 import demo3 from '../../images/demo3.png';
 import demo4 from '../../images/demo4.png';
 import demo5 from '../../images/demo5.png';
+import demo6 from '../../images/AdminPanel2.png';
 
 
 class  Portfolio extends Component{
@@ -12,20 +13,27 @@ class  Portfolio extends Component{
         demos:[
             {
                 id:1,
+                projectName:"Admin Panel (Theme)",
+                imageSrc:`url(${demo6})`,
+                demoUrl:"https://admin-panel2.netlify.app/",
+                codeUrl:"https://github.com/madinajabrayil/AdminPanel2"
+            },
+            {
+                id:2,
                 projectName:"Design and Develop",
                 imageSrc:`url(${demo1})`,
                 demoUrl:"https://deploy-project.netlify.app",
                 codeUrl:"https://github.com/madinajabrayil/project"
             },
             {
-                id:2,
+                id:3,
                 projectName:"Starbucks Landing Page",
                 imageSrc:`url(${demo2})`,
                 demoUrl:"https://starbucks-page.netlify.app",
                 codeUrl:"https://github.com/madinajabrayil/starbucks-landing-page"
             },
             {
-                id:3,
+                id:4,
                 projectName:"IMAJH E-commerce",
                 imageSrc:`url(${demo3})`,
                 demoUrl:"https://imajh-e-commerce.netlify.app",
@@ -33,14 +41,14 @@ class  Portfolio extends Component{
             },
            
             {
-                id:4,
+                id:5,
                 projectName:"Admin Panel",
                 imageSrc:`url(${demo4})`,
                 demoUrl:"https://adminpanel1.netlify.app/index.html",
                 codeUrl:"https://github.com/madinajabrayil/AdminPanel"
             },
             {
-                id:5,
+                id:6,
                 projectName:"Dashboard",
                 imageSrc:`url(${demo5})`,
                 demoUrl:"https://dashboard-project1.netlify.app/",
